ce16254afb1d65ac39d96d30cd53bdd054b743df
yairs <yair.sanes@gmail.com>
2021-04-21 14:41:07 +0300

    visDrone data loader (work in progress)

--- mega_core/data/datasets/visDroneDataLoader.py
+++ mega_core/data/datasets/visDroneDataLoader.py
@@ -1,42 +1,42 @@
 import os
+import numpy as np
+import torch
 import pickle
 
-import torch
-import torch.utils.data
-
-from PIL import Image
+import pandas as pd
 import cv2
+from tqdm import tqdm
 import sys
-import numpy as np
 
-if sys.version_info[0] == 2:
-    import xml.etree.cElementTree as ET
-else:
-    import xml.etree.ElementTree as ET
-
-from mega_core.structures.bounding_box import BoxList
 from mega_core.utils.comm import is_main_process
+from mega_core.data.datasets.vid import VIDDataset
 
+class VisDroneDataset(VIDDataset):
 
-class VIDDataset(torch.utils.data.Dataset):
-    classes = ['__background__',  # always index 0
-                'airplane', 'antelope', 'bear', 'bicycle',
-                'bird', 'bus', 'car', 'cattle',
-                'dog', 'domestic_cat', 'elephant', 'fox',
-                'giant_panda', 'hamster', 'horse', 'lion',
-                'lizard', 'monkey', 'motorcycle', 'rabbit',
-                'red_panda', 'sheep', 'snake', 'squirrel',
-                'tiger', 'train', 'turtle', 'watercraft',
-                'whale', 'zebra']
-    classes_map = ['__background__',  # always index 0
-                    'n02691156', 'n02419796', 'n02131653', 'n02834778',
-                    'n01503061', 'n02924116', 'n02958343', 'n02402425',
-                    'n02084071', 'n02121808', 'n02503517', 'n02118333',
-                    'n02510455', 'n02342885', 'n02374451', 'n02129165',
-                    'n01674464', 'n02484322', 'n03790512', 'n02324045',
-                    'n02509815', 'n02411705', 'n01726692', 'n02355227',
-                    'n02129604', 'n04468005', 'n01662784', 'n04530566',
-                    'n02062744', 'n02391049']
+    # classes = ['__background__',  # always index 0
+    #             'airplane', 'antelope', 'bear', 'bicycle',
+    #             'bird', 'bus', 'car', 'cattle',
+    #             'dog', 'domestic_cat', 'elephant', 'fox',
+    #             'giant_panda', 'hamster', 'horse', 'lion',
+    #             'lizard', 'monkey', 'motorcycle', 'rabbit',
+    #             'red_panda', 'sheep', 'snake', 'squirrel',
+    #             'tiger', 'train', 'turtle', 'watercraft',
+    #             'whale', 'zebra']
+
+    classes = [
+        {'id': 0, 'name': 'ignored-regions'},
+        {'id': 1, 'name': 'pedestrian'},
+        {'id': 2, 'name': 'people'},
+        {'id': 3, 'name': 'bicycle'},
+        {'id': 4, 'name': 'car'},
+        {'id': 5, 'name': 'van'},
+        {'id': 6, 'name': 'truck'},
+        {'id': 7, 'name': 'tricycle'},
+        {'id': 8, 'name': 'awning-tricycle'},
+        {'id': 9, 'name': 'bus'},
+        {'id': 10, 'name': 'motor'},
+        {'id': 11, 'name': 'others'},
+    ]
 
     def __init__(self, image_set, data_dir, img_dir, anno_path, img_index, transforms, is_train=True):
         self.det_vid = image_set.split("_")[0]
@@ -81,82 +81,49 @@ class VIDDataset(torch.utils.data.Dataset):
         self.classes_to_ind = dict(zip(self.classes_map, range(len(self.classes_map))))
         self.categories = dict(zip(range(len(self.classes)), self.classes))
 
+        self.classes = [
+            {'id': 0, 'name': 'ignored-regions'},
+            {'id': 1, 'name': 'pedestrian'},
+            {'id': 2, 'name': 'people'},
+            {'id': 3, 'name': 'bicycle'},
+            {'id': 4, 'name': 'car'},
+            {'id': 5, 'name': 'van'},
+            {'id': 6, 'name': 'truck'},
+            {'id': 7, 'name': 'tricycle'},
+            {'id': 8, 'name': 'awning-tricycle'},
+            {'id': 9, 'name': 'bus'},
+            {'id': 10, 'name': 'motor'},
+            {'id': 11, 'name': 'others'},
+        ]
+
         self.annos = self.load_annos(os.path.join(self.cache_dir, self.image_set + "_anno.pkl"))
 
-    def __getitem__(self, idx):
-        if self.is_train:
-            return self._get_train(idx)
-        else:
-            return self._get_test(idx)
 
-    def _get_train(self, idx):
-        filename = self.image_set_index[idx]
-        img = Image.open(self._img_dir % filename).convert("RGB")
+    def _preprocess_annotation(self, frame_anno , video_size): #frame_anno is a file in annotations (linked to a video)
+        boxes = []  # a list of lists
+        gt_classes = []  # list
 
-        target = self.get_groundtruth(idx)
-        target = target.clip_to_image(remove_empty=True)
+        im_info = video_size
 
-        if self.transforms is not None:
-            img, target = self.transforms(img, target)
 
-        return img, target, idx
 
-    def _get_test(self, idx):
-        return self._get_train(idx)
+        for target in frame_anno.iterrows():
+            target_id = int(target[1][1])
+            bbox_left = int(target[1][2])
+            bbox_top = int(target[1][3])
+            bbox_right = bbox_left+int(target[1][4])
+            bbox_bottom = bbox_top+int(target[1][4])
+            object_class_num = int(target[1][7])
 
-    def __len__(self):
-        return len(self.image_set_index)
-
-    def filter_annotation(self):
-        cache_file =os.path.join(self.cache_dir, self.image_set + "_keep.pkl")
-
-        if os.path.exists(cache_file):
-            with open(cache_file, "rb") as fid:
-                keep = pickle.load(fid)
-            if is_main_process():
-                print("{}'s keep information loaded from {}".format(self.det_vid, cache_file))
-            return keep
-
-        keep = np.zeros((len(self)), dtype=np.bool)
-        for idx in range(len(self)):
-            if idx % 10000 == 0:
-                print("Had filtered {} images".format(idx))
-
-            filename = self.image_set_index[idx]
-
-            tree = ET.parse(self._anno_path % filename).getroot()
-            objs = tree.findall("object")
-            keep[idx] = False if len(objs) == 0 else True
-        print("Had filtered {} images".format(len(self)))
-
-        if is_main_process():
-            with open(cache_file, "wb") as fid:
-                pickle.dump(keep, fid)
-            print("Saving {}'s keep information into {}".format(self.det_vid, cache_file))
-
-        return keep
-
-    def _preprocess_annotation(self, target):
-        boxes = []
-        gt_classes = []
-
-        size = target.find("size")
-        im_info = tuple(map(int, (size.find("height").text, size.find("width").text)))
-
-        objs = target.findall("object")
-        for obj in objs:
-            if not obj.find("name").text in self.classes_to_ind:
-                continue
-
-            bbox =obj.find("bndbox")
             box = [
-                np.maximum(float(bbox.find("xmin").text), 0),
-                np.maximum(float(bbox.find("ymin").text), 0),
-                np.minimum(float(bbox.find("xmax").text), im_info[1] - 1),
-                np.minimum(float(bbox.find("ymax").text), im_info[0] - 1)
+                bbox_left,
+                bbox_top,
+                bbox_right,
+                bbox_bottom
             ]
             boxes.append(box)
-            gt_classes.append(self.classes_to_ind[obj.find("name").text.lower().strip()])
+
+            gt_classes.append(self.classes[object_class_num]['name'])
 
         res = {
             "boxes": torch.tensor(boxes, dtype=torch.float32).reshape(-1, 4),
@@ -165,67 +132,85 @@ class VIDDataset(torch.utils.data.Dataset):
         }
         return res
 
+
     def load_annos(self, cache_file):
-        if os.path.exists(cache_file):
-            with open(cache_file, "rb") as fid:
-                annos = pickle.load(fid)
-            if is_main_process():
-                print("{}'s annotation information loaded from {}".format(self.det_vid, cache_file))
-        else:
-            annos = []
-            for idx in range(len(self)):
-                if idx % 10000 == 0:
-                    print("Had processed {} images".format(idx))
 
-                filename = self.image_set_index[idx]
+        # video_name = 'uav0000013_00000_v'
+        ann_dir = f'C:/Library/School/Project/visDrone/VisDrone2019-VID-train/annotations'
+        # ann_file = f'C:/Library/School/Project/visDrone/VisDrone2019-VID-train/annotations/{video_name}.txt'
 
-                tree = ET.parse(self._anno_path % filename).getroot()
-                anno = self._preprocess_annotation(tree)
+        annos = []
+
+        for ann_file in os.listdir(ann_dir):
+            video_name = ann_file.rstrip(".txt")
+            image_dir = ann_dir.rstrip("annotations")+f'sequences/{video_name}'
+
+            # image_dir = f'C:/Library/School/Project/visDrone/VisDrone2019-VID-train/sequences/{video_name}'
+
+            image_name = '0000001.jpg'
+            image_path = os.path.join(image_dir, image_name)
+            image = cv2.imread(image_path)
+            im_size = tuple(map(int, (image.shape[0].text, image.shape[1].text)))
+
+            ann_df = pd.read_csv(ann_file)
+            ann_df.columns = ['frame_index', 'target_id', 'bbox_left', 'bbox_top', 'bbox_width', 'bbox_height', 'score', 'object_category', 'truncation', 'occlusion']
+            frame_id_list = sorted(ann_df.loc[:, 'frame_index'].unique().tolist())
+
+
+            for frame_id in tqdm(frame_id_list):
+                mask = ann_df.loc[:, 'frame_index'] == frame_id
+                df = ann_df.loc[mask, :]
+                anno = self._preprocess_annotation(df, im_size)  # each anno belongs to a single frame
                 annos.append(anno)
-            print("Had processed {} images".format(len(self)))
-
-            if is_main_process():
-                with open(cache_file, "wb") as fid:
-                    pickle.dump(annos, fid)
-                print("Saving {}'s annotation information into {}".format(self.det_vid, cache_file))
 
         return annos
 
-    def get_img_info(self, idx):
-        im_info = self.annos[idx]["im_info"]
-        return {"height": im_info[0], "width": im_info[1]}
 
-    @property
-    def cache_dir(self):
-        """
-        make a directory to store all caches
-        :return: cache path
-        """
-        cache_dir = os.path.join(self.data_dir, 'cache')
-        if not os.path.exists(cache_dir):
-            os.mkdir(cache_dir)
-        return cache_dir
 
-    def get_visualization(self, idx):
-        filename = self.image_set_index[idx]
+    """
+    def _preprocess_annotation(self, video_target , video_size): #video_target is a file in annotations (linked to a video)
+        boxes = []  # a list of lists
+        gt_classes = []  # list
 
-        img = cv2.imread(self._img_dir % filename)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-        target = self.get_groundtruth(idx)
-        target = target.clip_to_image(remove_empty=True)
 
-        return img, target, filename
+        # size = target.find("size")
+        # im_info = tuple(map(int, (size.find("height").text, size.find("width").text)))
 
-    def get_groundtruth(self, idx):
-        anno = self.annos[idx]
+        im_info = video_size
 
-        height, width = anno["im_info"]
-        target = BoxList(anno["boxes"].reshape(-1, 4), (width, height), mode="xyxy")
-        target.add_field("labels", anno["labels"])
+        ann_df = pd.read_csv(video_target)
+        ann_df.columns = ['frame_index', 'target_id', 'bbox_left', 'bbox_top', 'bbox_width', 'bbox_height', 'score', 'object_category', 'truncation', 'occlusion']
+        frame_id_list = sorted(ann_df.loc[:, 'frame_index'].unique().tolist())
 
-        return target
+        for frame_id in tqdm(frame_id_list):
+            mask = ann_df.loc[:, 'frame_index'] == frame_id
+            df = ann_df.loc[mask, :]
 
-    @staticmethod
-    def map_class_id_to_class_name(class_id):
-        return VIDDataset.classes[class_id]
+            for target in df.iterrows():
+                target_id = int(target[1][1])
+                bbox_left = int(target[1][2])
+                bbox_top = int(target[1][3])
+                bbox_right = bbox_left+int(target[1][4])
+                bbox_bottom = bbox_top+int(target[1][4])
+                object_class_num = int(target[1][7])
+
+                box = [
+                    bbox_left,
+                    bbox_top,
+                    bbox_right,
+                    bbox_bottom
+                ]
+                boxes.append(box)
+
+                gt_classes.append(self.classes[object_class_num]['name'])
+                # gt_classes.append(self.classes_to_ind[obj.find("name").text.lower().strip()])
+
+        res = {
+            "boxes": torch.tensor(boxes, dtype=torch.float32).reshape(-1, 4),
+            "labels": torch.tensor(gt_classes),
+            "im_info": im_info,
+        }
+        return res
+
+    """
